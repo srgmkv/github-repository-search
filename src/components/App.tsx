a8108c260d8af71cf6184c187836b40d8044c354
srgmkv <ms5533@yandex.ru>
2019-07-20 01:02:00 +0700

    debounce repaired

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from './Header/Header';
-import SearchBar from '../containers/SearchBar/Searchbar';
+import SearchBar from '../containers/SearchBar/SearchBar';
 import MainSection from './MainSection/MainSection';
 
 
--- src/containers/SearchBar/SearchBar.tsx
+++ src/containers/SearchBar/SearchBar.tsx
@@ -5,40 +5,28 @@ import { IState } from '../../models';
 import './SearchBar.css';
 import { changeInput, fetchRepos } from '../../actions/index';
 
-interface Props extends InputValue {
+interface Props {
   changeInput: typeof changeInput
   fetchRepos: typeof fetchRepos
 }
 
-interface InputValue {
-  inputValue: string
-}
-
-const SearchBar = ({ inputValue, changeInput, fetchRepos }: Props) => {
+const SearchBar = ({ changeInput, fetchRepos }: Props) => {
 
   const debouncedFetchRepos = debounce(fetchRepos, 1000);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value }: { value: string } = e.target;
     changeInput(value);
     value.length > 2 && debouncedFetchRepos(value);
-    console.log('inputValue', inputValue)
-
   }
-
 
   return (
     <div className='input'>
       <input type="text" id="text-input"
         onChange={handleChange}
-        value={inputValue}
       />
     </div>
   );
 }
 
-const mapStateToProps = (state: IState): InputValue => ({
-  inputValue: state.inputValue
-});
-
-export default connect(mapStateToProps, { changeInput, fetchRepos })(SearchBar);
+export default connect(null, { changeInput, fetchRepos })(SearchBar);
