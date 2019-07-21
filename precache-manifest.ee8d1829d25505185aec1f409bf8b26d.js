6a963d54d4f13b5146850963ba95b9a828b12011
srgmkv <ms5533@yandex.ru>
2019-07-22 01:38:05 +0700

    Updates

---
+++ precache-manifest.ee8d1829d25505185aec1f409bf8b26d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ececbd7d17d65e9a4deca803d28864",
+    "url": "/github-repository-search/index.html"
+  },
+  {
+    "revision": "5a23021e08d243cc1ff9",
+    "url": "/github-repository-search/static/css/main.4f6a707d.chunk.css"
+  },
+  {
+    "revision": "9dfd72baa8545d7f45b4",
+    "url": "/github-repository-search/static/js/2.32e4532e.chunk.js"
+  },
+  {
+    "revision": "5a23021e08d243cc1ff9",
+    "url": "/github-repository-search/static/js/main.54d5b892.chunk.js"
+  },
+  {
+    "revision": "b468ab77fabcc1aa8a55",
+    "url": "/github-repository-search/static/js/runtime~main.f961832c.js"
+  },
+  {
+    "revision": "75895134184a9930f6779ec41182dc60",
+    "url": "/github-repository-search/static/media/github.75895134.svg"
+  },
+  {
+    "revision": "9f84c544e2064ab36c049aa604ef9cd5",
+    "url": "/github-repository-search/static/media/github2.9f84c544.svg"
+  },
+  {
+    "revision": "b48a01fa21b54a3894159c4989704d63",
+    "url": "/github-repository-search/static/media/loader5.b48a01fa.svg"
+  },
+  {
+    "revision": "385d7b6e0d70037751ea3ec76dde51e6",
+    "url": "/github-repository-search/static/media/star.385d7b6e.svg"
+  },
+  {
+    "revision": "6a7e791db6af78f90f79f8c607f79391",
+    "url": "/github-repository-search/static/media/watcher.6a7e791d.svg"
+  }
+]);
